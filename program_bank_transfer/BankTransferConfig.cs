6ec69145f1e028f04b73fe01e440ba681f4d1ed0
Syahreza Adnan Al Azhar <notsupersyahreza@student.telkomuniversity.ac.id>
2024-04-17 07:05:50 +0700

    menambahkan class BankTransferConfig

---
+++ program_bank_transfer/BankTransferConfig.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text.Json;
+
+public class BankTransferConfig
+{
+	public class tf
+	{
+		public int threshold { get; set; }
+		public int low_fee { get; set; }
+        public int high_fee { get; set; }
+    }
+	public class conf
+	{
+		public string en { get; set; }
+        public string id { get; set; }
+    }
+	public string lang { get; set; }
+	public tf transfer { get; set; }
+	public string[] methods { get; set; }
+	public conf confirmation { get; set; }
+
+    public BankTransferConfig()
+	{
+		lang = "en";
+		transfer.threshold = 25000000;
+		transfer.low_fee = 6500;
+		transfer.high_fee = 15000;
+		methods = new string[] { "RTO (real-time)", "SKN", "RTGS", "BI FAST" };
+		confirmation.en = "yes";
+		confirmation.id = "ya";
+	}
+
+    public void ReadConfigFile(string path)
+    {
+        if (File.Exists(path))
+        {
+            string jsonText = File.ReadAllText(path);
+            BankTransferConfig config = JsonSerializer.Deserialize<BankTransferConfig>(jsonText);
+            this.lang = config.lang;
+            this.transfer = config.transfer;
+            this.methods = config.methods;
+            this.confirmation = config.confirmation;
+        }
+        else
+        {
+            Console.WriteLine("File config tidak ditemukan.");
+        }
+    }
+
+    public void WriteConfigFile(string path)
+    {
+        string jsonText = JsonSerializer.Serialize(this);
+        File.WriteAllText(path, jsonText);
+        Console.WriteLine("File config berhasil ditulis.");
+    }
+
+    public void ReadAndWriteConfigFile(string path)
+    {
+        if (File.Exists(path))
+        {
+            ReadConfigFile(path);
+        }
+        else
+        {
+            WriteConfigFile(path);
+            ReadConfigFile(path);
+        }
+    }
+}
